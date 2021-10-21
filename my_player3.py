edaa13c54ab923c8f8e786302a2d4e7a1b1ba824
jojuno <35975923+jojuno@users.noreply.github.com>
2021-10-20 20:18:12 -0700

    erased unnecessary comments

--- my_player3.py
+++ my_player3.py
@@ -135,28 +135,6 @@ class MinimaxPlayer():
             else:
                 return random.choice(possible_placements)
 
-    '''
-    def add_children(self, node, piece_type): #exclude depth for now
-        current_state = deepcopy(node.data) #make a deep copy of the current state
-        parent = Node(current_state) #go instead of go.board so you can leverage the member functions
-        for i in range(current_state.size):
-            for j in range(current_state.size):
-                if current_state.valid_place_check(i, j, piece_type, test_check = True):
-                    next_state = deepcopy(current_state)
-                    next_state.place_chess(i, j, piece_type)
-                    next_state.remove_died_pieces(3-piece_type)
-                    child = Node(next_state)
-                    parent.addChild(child)
-        return parent
-    
-    def build_minimax_tree_3(self, go, piece_type, depth):
-        current_state = deepcopy(go) #make a deep copy of the current state
-        root = Node(current_state) #go instead of go.board so you can leverage the member functions
-        for level in range(depth):
-            if level == 0:
-                root = self.add_children(self, root, piece_type)
-    '''
-
     #this will always be in the perspective of my_player
     def evaluate(self, state):
         pass
@@ -180,120 +158,6 @@ class MinimaxPlayer():
                     root.addChild(child)
                     print("depth", depth, ", move", i, j)
                     self.build_minimax_tree_recursive(root, 3-piece_type, depth_max, depth+1)
-        
-
-
-    '''
-    def build_minimax_tree_2(self, go, piece_type, depth):
-        #add current state as root
-        #add possible states as children
-        #state, action to get to that state, and evaluation score
-        current_state = deepcopy(go) #make a deep copy of the current state
-        root = Node(current_state) #go instead of go.board so you can leverage the member functions
-
-        for level in range(depth):
-            if level == 0:
-                for i in range(current_state.size):
-                    for j in range(current_state.size):
-                        if current_state.valid_place_check(i, j, piece_type, test_check = True):
-                            next_state = deepcopy(current_state)
-                            next_state.place_chess(i, j, piece_type)
-                            next_state.remove_died_pieces(3-piece_type)
-                            child = Node(next_state)
-                            root.addChild(child)
-            elif level == 1: 
-                for possible_state in root.children:
-                    for i in range(possible_state.size):
-                        for j in range(possible_state.size):
-                            if possible_state.valid_place_check(i, j, 3-piece_type, test_check = True): #min's turn
-                                next_state = deepcopy(possible_state)
-                                next_state.place_chess(i, j, 3-piece_type)
-                                next_state.remove_died_pieces(piece_type) #remove max's stones
-                                child = Node(next_state)
-                                possible_state.addChild(child)
-            elif level == 2:
-                for possible_state in 
-    '''
-
-    '''
-    read a minimax tree that's built for the entire game
-    '''
-    '''
-    def read_minimax_tree(self, piece_type):
-        max_evaluation = -1
-        move = [-1, -1]
-        if piece_type == 1: #X
-            f = open("minimax_tree_x.txt", "r")
-            lines = f.readlines()
-        elif piece_type == 2:
-            f = open("minimax_tree_o.txt", "r")
-            lines = f.readlines()
-        for line_num in range(len(lines)):
-            line = lines[line_num].split()
-            print(line)
-            if line[0] == "evaluation":
-                evaluation = float(line[1])
-                if evaluation > max_evaluation:
-                    max_evaluation = evaluation
-                    move_x = lines[line_num+1].split()[1]
-                    move_y = lines[line_num+1].split()[2]
-                    move = [move_x, move_y]
-        print("best move: ", str(move[0]), str(move[1]))
-        return move
-    '''
-
-    '''
-    build a minimax tree for the entire game
-    according to the TA, this combines minimax and q-learning
-    '''
-    '''
-    #current state is at depth 0
-    def build_minimax_tree(self, go, max, depth):
-        if max == 1:
-            f = open("minimax_tree_x.txt", "w")
-        elif max == 2:
-            f = open("minimax_tree_o.txt", "w")
-        
-        piece_type = max
-        
-        possible_go = GO(5)
-        original_stdout = sys.stdout
-        #continue building until you run into a terminal state
-        parent = 0
-        node_number = 0
-        for level in range(depth):
-            max_evaluation = 0
-            max_evaluation_move = []
-            if parent == 0:
-                node_number = 1
-            f.write("depth " + str(depth) + "\n")
-            f.write("node number " + str(node_number) + ", parent number " + str(parent) + "\n")
-            
-            sys.stdout = f
-            go.visualize_board()
-            print("starting state")
-            parent = node
-            
-            for i in range(go.size):
-                for j in range(go.size):
-                    if go.valid_place_check(i, j, piece_type, test_check = True):
-                        possible_go.board = deepcopy(go.board)
-                        possible_go.place_chess(i, j, piece_type)
-                        possible_go.remove_died_pieces(3-piece_type)
-                        possible_go.visualize_board()
-                        node_number += 1
-                        evaluation = possible_go.judge_winner()
-                        if evaluation == piece_type:
-                            evaluation = 1
-                        elif evaluation == 2:
-                            evaluation = 0
-                        else:
-                            evaluation = 0.5
-                        f.write("node number " + str(node_number) + ", parent number " + str(parent) + "\n")
-                        f.write("evaluation " + str(evaluation) + "\n")
-                        print("move:", i, j)
-        sys.stdout = original_stdout
-    '''
 
 class RandomPlayer():
     def __init__(self):
@@ -307,9 +171,7 @@ class RandomPlayer():
         :param go: Go instance.
         :param piece_type: 1('X') or 2('O').
         :return: (row, column) coordinate of input.
-        '''        
-
-
+        '''       
 
         possible_placements = []
         for i in range(go.size):
